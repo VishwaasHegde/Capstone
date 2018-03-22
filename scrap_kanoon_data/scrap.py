1f3a77e1e06c37537fa6dfa022d3a41783f94e4c
uday <uday1989soft@gmail.com>
2018-03-22 12:24:59 +0530

    scraping judgement URL IDS

---
+++ scrap_kanoon_data/scrap.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 17:31:00 2018
+
+@author: ram
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+import time
+
+def appendDocumentList(listofURLS):
+    global alldocumentIDs
+    for i in listofURLS:
+        try:
+            documentID = str(i).split('href="/docfragment/')[1].split('/')[0]
+            alldocumentIDs.append(documentID)
+        except:
+            print (i)
+            pass
+
+
+alldocumentIDs = []
+FILEPATH = r'urllist.txt'
+with open(FILEPATH) as urls:
+    try:
+        for url in urls:
+            r = requests.get(url)
+            try:
+                soup = BeautifulSoup(r.content, "lxml")
+            except:
+                time.sleep(240)
+                r = requests.get(url)    
+                soup = BeautifulSoup(r.content, "lxml")
+            try:
+                totalPages = int(int(str(soup.find_all(class_="results_middle")[0]).split('<div><b>')[1].split('</b>')[0].split('of')[1].strip())/10)
+                appendDocumentList(soup.find_all(class_="result_title"))
+                for i in range(totalPages):
+                    time.sleep(5)
+                    curl=((str(url)+'&pagenum='+str((i+1))).replace('\n', '').replace('\r', ''))
+                    r = requests.get(curl)
+                    try:
+                        soup = BeautifulSoup(r.content, "lxml")
+                        appendDocumentList( soup.find_all(class_="result_title"))
+                    except:
+                        time.sleep(240)
+                        print("Unexpected error: -> ", sys.exc_info()[0])
+                        pass
+            except:
+                print("Unexpected error    :", sys.exc_info()[0])
+                pass
+    except:
+        print("Unexpected error    :", sys.exc_info()[0])
+        pass
+
+## writing document URLS to File
+file = open('allDocumentsURLS.txt','w') 
+for docid in alldocumentIDs:
+    file.write('https://indiankanoon.org/doc/'+str(docid))
+    file.write('\n')
+file.close()
+#
+#id = alldocumentIDs[0]
+#r = requests.get('https://indiankanoon.org/doc/'+str(id))
+#soup = BeautifulSoup(r.content, "lxml")
+#print(soup)
